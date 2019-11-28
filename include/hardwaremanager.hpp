c302ca2f03c0080f0ac21f65d8dc11f78d88f745
Richard Edgar <freesurfer.rge@gmail.com>
2019-11-28 10:30:01 -0500

    Some extra documentation

--- include/hardwaremanager.hpp
+++ include/hardwaremanager.hpp
@@ -9,11 +9,20 @@
 namespace Lineside {
   //! Type for objects which supply binary output pins
   typedef HardwareProvider<BinaryOutputPin> BOPProvider;
+
+  //! Type for objects which supply binary input pins
   typedef HardwareProvider<BinaryInputPin> BIPProvider;
+
+  //! Type for objects which supply PWM channels
   typedef HardwareProvider<PWMChannel> PWMCProvider;
-  
+
+  //! Registrar for BOPProvider objects
   typedef Registrar<BOPProvider> BOPProviderRegistrar;
+
+  //! Registrar for BIPProvider objects
   typedef Registrar<BIPProvider> BIPProviderRegistrar;
+
+  //! Registrar for PWMCProvider objects
   typedef Registrar<PWMCProvider> PWMCProviderRegistrar;
   
   //! Abstract class for managing access to hardware
