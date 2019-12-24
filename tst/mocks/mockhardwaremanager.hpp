0d09f67112115cd89719493c3631c38144a38033
Richard Edgar <freesurfer.rge@gmail.com>
2019-12-24 09:26:53 -0500

    Starting to think more about the mock hardware manager

--- tst/mocks/mockhardwaremanager.hpp
+++ tst/mocks/mockhardwaremanager.hpp
@@ -2,8 +2,23 @@
 
 #include "hardwaremanager.hpp"
 
+#include "mockpwmchannelprovider.hpp"
+
 class MockHardwareManager : public Lineside::HardwareManager {
 public:
+  const std::string PWMChannelProviderId = "PWM";
+  
+  MockHardwareManager() :
+    HardwareManager(),
+    pwmChannelProvider(std::make_shared<MockPWMChannelProvider>()),
+    pwmChannelProviderRegistrar(std::make_shared<Lineside::PWMCProviderRegistrar>()) {
+    this->pwmChannelProviderRegistrar->Register( PWMChannelProviderId, this->pwmChannelProvider );
+  }
+  
+  std::shared_ptr<MockPWMChannelProvider> pwmChannelProvider;
+  
+  std::shared_ptr<Lineside::PWMCProviderRegistrar> pwmChannelProviderRegistrar;
+  
   virtual std::weak_ptr<Lineside::BOPProviderRegistrar> GetBOPProviderRegistrar() override;
   virtual std::weak_ptr<Lineside::BIPProviderRegistrar> GetBIPProviderRegistrar() override;
   virtual std::weak_ptr<Lineside::PWMCProviderRegistrar> GetPWMCProviderRegistrar() override;
