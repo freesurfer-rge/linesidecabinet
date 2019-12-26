6b39459fdfdd6bd928062fbf9f06db3397f7a80a
Richard Edgar <freesurfer.rge@gmail.com>
2019-12-26 14:47:29 -0500

    Starting to put together the mock BOP provider

---
+++ tst/mocks/mockbopprovider.cpp
@@ -0,0 +1,8 @@
+#include "linesideexceptions.hpp"
+
+#include "mockbopprovider.hpp"
+
+std::weak_ptr<Lineside::BinaryOutputPin> MockBOPProvider::GetHardware(const std::string& hardwareId,
+								      const std::map<std::string,std::string>& settings) {
+  
+}
