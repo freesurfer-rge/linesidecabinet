e3a2e23b435561a3eb172fdf818de4dafabf2602
Richard Edgar <freesurfer.rge@gmail.com>
2020-01-04 12:45:27 -0500

    Hooking in to mock hardware manager

--- tst/mocks/mockhardwaremanager.hpp
+++ tst/mocks/mockhardwaremanager.hpp
@@ -2,27 +2,32 @@
 
 #include "hardwaremanager.hpp"
 
+#include "mockbipprovider.hpp"
 #include "mockbopprovider.hpp"
 #include "mockpwmchannelprovider.hpp"
 
 class MockHardwareManager : public Lineside::HardwareManager {
 public:
+  const std::string BIPProviderId = "BIP";
   const std::string BOPProviderId = "BOP";
   const std::string PWMChannelProviderId = "PWM";
   
   MockHardwareManager() :
     HardwareManager(),
+    bipProvider(std::make_shared<MockBIPProvider>()),
     bopProvider(std::make_shared<MockBOPProvider>()),
-    pwmChannelProvider(std::make_shared<MockPWMChannelProvider>()),
+    pwmChannelProvider(std::make_shared<MockPWMChannelProvider>())
     bopProviderRegistrar(std::make_shared<Lineside::BOPProviderRegistrar>()),
     pwmChannelProviderRegistrar(std::make_shared<Lineside::PWMCProviderRegistrar>()) {
     this->bopProviderRegistrar->Register( BOPProviderId, this->bopProvider );
     this->pwmChannelProviderRegistrar->Register( PWMChannelProviderId, this->pwmChannelProvider );
   }
 
+  std::shared_ptr<MockBIPProvider> bipProvider;
   std::shared_ptr<MockBOPProvider> bopProvider;
   std::shared_ptr<MockPWMChannelProvider> pwmChannelProvider;
 
+  std::shared_ptr<Lineside::BIPProviderRegistrar> bipProviderRegistrar;
   std::shared_ptr<Lineside::BOPProviderRegistrar> bopProviderRegistrar;
   std::shared_ptr<Lineside::PWMCProviderRegistrar> pwmChannelProviderRegistrar;
   
