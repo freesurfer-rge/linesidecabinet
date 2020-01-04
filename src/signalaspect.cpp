85efa33f78d80251456222f9ebe87d767cb11584
Richard Edgar <freesurfer.rge@gmail.com>
2020-01-03 19:11:35 -0500

    Get everything working

--- src/signalaspect.cpp
+++ src/signalaspect.cpp
@@ -4,6 +4,16 @@
 #include "signalaspect.hpp"
 
 namespace Lineside {
+  static boost::bimap<SignalAspect,std::string> convertor;
+
+  static void initconvertor() {
+    typedef decltype(convertor)::value_type pos;
+    convertor.insert( pos(SignalAspect::Red, "Red") );
+    convertor.insert( pos(SignalAspect::DoubleYellow, "DoubleYellow") );
+    convertor.insert( pos(SignalAspect::Yellow, "Yellow") );
+    convertor.insert( pos(SignalAspect::Green, "Green") );
+  }
+  
   std::ostream& operator<<( std::ostream& os, const SignalAspect s ) {
     os << ToString( s );
     
@@ -11,9 +21,21 @@ namespace Lineside {
   }
 
   std::string ToString( const SignalAspect s ) {
-    if( s != SignalAspect::Red ) {
-      throw std::logic_error(__PRETTY_FUNCTION__);
+    if( convertor.empty() ) {
+      initconvertor();
     }
-    return std::string();
+
+    std::string res;
+    try {
+      res = convertor.left.at(s);
+    }
+    catch( std::out_of_range& e ) {
+      std::stringstream msg;
+      msg << "Unrecognised SignalAspect: ";
+      msg << static_cast<int>(s);
+      throw std::runtime_error(msg.str());
+    }
+
+    return res;
   }
 }
--- tst/signalaspecttests.cpp
+++ tst/signalaspecttests.cpp
@@ -2,6 +2,8 @@
 #include <boost/test/data/test_case.hpp>
 #include <boost/test/data/monomorphic.hpp>
 
+#include <sstream>
+
 #include "signalaspect.hpp"
 
 char const* names[] = { "Red", "DoubleYellow", "Yellow", "Green" };
@@ -13,12 +15,22 @@ Lineside::SignalAspect aspects[] = { Lineside::SignalAspect::Red,
 BOOST_AUTO_TEST_SUITE( SignalAspect )
 
 
-BOOST_DATA_TEST_CASE( ToStringCheck,
+BOOST_DATA_TEST_CASE( ToString,
 		      boost::unit_test::data::make(names)^boost::unit_test::data::make(aspects),
 		      name,
 		      aspect )
 {
   BOOST_CHECK_EQUAL( name, Lineside::ToString(aspect) );
+}
+
+BOOST_DATA_TEST_CASE( StreamInsertion,
+		      boost::unit_test::data::make(names)^boost::unit_test::data::make(aspects),
+		      name,
+		      aspect )
+{
+  std::stringstream res;
+  res << aspect;
+  BOOST_CHECK_EQUAL( res.str(), name );
 }
 
 
