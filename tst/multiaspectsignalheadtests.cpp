b959598e109f9cbd7dba2ff5d0a4dd0499df6b81
Richard Edgar <freesurfer.rge@gmail.com>
2020-01-19 18:33:24 -0500

    Helps to add the right file

---
+++ tst/multiaspectsignalheadtests.cpp
@@ -0,0 +1,34 @@
+#include <boost/test/unit_test.hpp>
+
+#include "multiaspectsignalheaddata.hpp"
+#include "multiaspectsignalhead.hpp"
+
+#include "mockhardwaremanagerfixture.hpp"
+
+BOOST_FIXTURE_TEST_SUITE(MultiAspectSignalHead, MockHardwareManagerFixture)
+
+BOOST_AUTO_TEST_CASE(ConstructTwoAspect)
+{
+  const Lineside::ItemId id(11);
+  const std::string controller = this->hwManager->BOPProviderId;
+  const std::string redData = "07";
+  const std::string greenData = "08";
+
+  Lineside::DeviceRequestData redRequest;
+  redRequest.controller = controller;
+  redRequest.controllerData = redData;
+
+  Lineside::DeviceRequestData greenRequest;
+  greenRequest.controller = controller;
+  greenRequest.controllerData = greenData;
+
+  Lineside::MultiAspectSignalHeadData mashd;
+  mashd.id = id;
+  mashd.aspectRequests[Lineside::SignalAspect::Red] = redRequest;
+  mashd.aspectRequests[Lineside::SignalAspect::Green] = greenRequest;
+
+  auto res = mashd.Construct( this->hwManager );
+  BOOST_REQUIRE( res );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
