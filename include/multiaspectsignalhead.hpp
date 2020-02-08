579d9d022b9e9f929186dbc6b3fd3774cb24fcc7
Richard Edgar <freesurfer.rge@gmail.com>
2020-02-08 15:45:43 -0500

    Add a line or two of documentation

--- include/multiaspectsignalhead.hpp
+++ include/multiaspectsignalhead.hpp
@@ -15,7 +15,8 @@
 
 namespace Lineside {
   class MultiAspectSignalHeadData;
-  
+
+  //! Implementation of a multiple aspect signal head
   class MultiAspectSignalHead : public PWItemModel {
   public:
     const std::chrono::milliseconds FlashInterval = std::chrono::milliseconds(500);
@@ -28,6 +29,7 @@ namespace Lineside {
 
     virtual bool HaveStateChange() override;
 
+    //! Set the desired state of the signal head
     void SetState(const SignalState wantedState,
 		  const SignalFlash wantedFlash,
 		  const unsigned int wantedFeather);
