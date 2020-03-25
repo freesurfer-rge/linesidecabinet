cccacb9a78f6c7f6dc36dce2187e347bb0bac527
Richard Edgar <freesurfer.rge@gmail.com>
2020-03-24 20:24:36 -0400

    Improve the Parse template

--- include/signalaspect.hpp
+++ include/signalaspect.hpp
@@ -6,10 +6,7 @@
 
 namespace Lineside {
   template<typename T>
-  T Parse(const std::string& src) {
-    throw std::logic_error(src);
-    static_assert(std::is_void<T>::value, "Must provide implementation");
-  }
+  T Parse(const std::string& src);
   
   enum class SignalAspect { Red,
 			    Yellow1,
