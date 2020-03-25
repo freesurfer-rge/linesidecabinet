6a155e792fab53a6cf0b37ae76b7613ebd94fe8a
Richard Edgar <freesurfer.rge@gmail.com>
2020-03-24 20:25:57 -0400

    Add a docstring

--- include/signalaspect.hpp
+++ include/signalaspect.hpp
@@ -5,6 +5,11 @@
 #include <type_traits>
 
 namespace Lineside {
+  //! The concept of parsing
+  /*!
+    There is no actual templated implementation. Individual
+    types must specialise the template for themselves.
+   */
   template<typename T>
   T Parse(const std::string& src);
   
