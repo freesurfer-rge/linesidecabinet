885153d538da10d7e6a503d7a67b3279623f1668
Richard Edgar <freesurfer.rge@gmail.com>
2020-04-01 17:46:11 -0400

    Split parse into separate file

--- include/signalaspect.hpp
+++ include/signalaspect.hpp
@@ -3,15 +3,10 @@
 #include <iostream>
 #include <string>
 
+#include "parse.hpp"
+
 namespace Lineside {
-  //! The concept of parsing
-  /*!
-    There is no actual templated implementation. Individual
-    types must specialise the template for themselves.
-   */
-  template<typename T>
-  T Parse(const std::string& src);
-  
+  //! Enumeration of allowed signal aspects
   enum class SignalAspect { Red,
 			    Yellow1,
 			    Yellow2,
@@ -22,6 +17,7 @@ namespace Lineside {
   
   std::string ToString( const SignalAspect s );
 
+  //! Template specialisation to parse a string to a SignalAspect
   template<>
   SignalAspect Parse<SignalAspect>(const std::string& src );
 }
