a98acf955842a542c79b7868d38ad954967de767
Richard Edgar <freesurfer.rge@gmail.com>
2020-04-10 19:03:37 -0400

    Starting work on a data format for the software manager

---
+++ include/softwaremanagerdata.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+#include <map>
+#include <string>
+
+namespace Lineside {
+  //! Class to hold data to construct a software manager
+  class SoftwareManagerData {
+  public:
+    std::string rtcAddress;
+    uint16_t rcPort;
+    std::map<std::string,std::string> settings;
+  };
+}
