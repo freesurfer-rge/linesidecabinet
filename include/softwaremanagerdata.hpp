6292f174300228b8fe170d7dd208053fc6037ae8
Richard Edgar <freesurfer.rge@gmail.com>
2020-04-10 19:45:21 -0400

    Need constructor for SoftwareManagerData

--- include/softwaremanagerdata.hpp
+++ include/softwaremanagerdata.hpp
@@ -8,6 +8,11 @@ namespace Lineside {
   //! Class to hold data to construct a software manager
   class SoftwareManagerData {
   public:
+    SoftwareManagerData() :
+      rtcAddress("Uninitialised"),
+      rtcPort(0),
+      settings() {}
+    
     std::string rtcAddress;
     uint16_t rtcPort;
     std::map<std::string,std::string> settings;
