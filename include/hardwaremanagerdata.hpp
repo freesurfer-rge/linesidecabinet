35ee6cc41625561f5d8e85ce3129b180c4f61651
Richard Edgar <freesurfer.rge@gmail.com>
2020-04-27 21:36:25 -0400

    Add constructor for HardwareManagerData

--- include/hardwaremanagerdata.hpp
+++ include/hardwaremanagerdata.hpp
@@ -10,6 +10,10 @@ namespace Lineside {
   //! Class to hold data for the hardware manager
   class HardwareManagerData {
   public:
+    HardwareManagerData() :
+      i2cDevices(),
+      settings() {}
+    
     std::vector<I2CDeviceData> i2cDevices;
     std::map<std::string,std::string> settings;
   };
