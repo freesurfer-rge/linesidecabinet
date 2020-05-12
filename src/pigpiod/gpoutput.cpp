0b7dc4c490c5360792feb18921ab737f5e1e26f0
Richard Edgar <freesurfer.rge@gmail.com>
2020-05-12 19:54:06 -0400

    Putting together a little more

--- src/pigpiod/gpoutput.cpp
+++ src/pigpiod/gpoutput.cpp
@@ -3,7 +3,10 @@
 namespace Lineside {
   namespace PiGPIOd {
     GPOutput::GPOutput(std::shared_ptr<GPIOPin> piPin) :
-      pin(piPin) {}
+      pin(piPin) {
+      this->pin->SetMode(GPIOMode::Output);
+      this->Set(false);
+    }
 
     void GPOutput::Set(const bool level) {
       this->pin->Write(level);
