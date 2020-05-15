ff362ce07b4c4add4db2e592642de8a870bfa451
Richard Edgar <freesurfer.rge@gmail.com>
2020-05-14 20:56:11 -0400

    Fix up tests for simplified hardware manager

--- tst/mocks/mockhardwaremanager.cpp
+++ tst/mocks/mockhardwaremanager.cpp
@@ -2,14 +2,3 @@
 
 #include "mockhardwaremanager.hpp"
 
-std::weak_ptr<Lineside::BOPProviderRegistrar> MockHardwareManager::GetBOPProviderRegistrar() {
-  return this->bopProviderRegistrar;
-}
-
-std::weak_ptr<Lineside::BIPProviderRegistrar> MockHardwareManager::GetBIPProviderRegistrar() {
-  return this->bipProviderRegistrar;
-}
-
-std::weak_ptr<Lineside::PWMCProviderRegistrar> MockHardwareManager::GetPWMCProviderRegistrar() {
-  return this->pwmChannelProviderRegistrar;
-}
--- tst/mocks/mockhardwaremanager.hpp
+++ tst/mocks/mockhardwaremanager.hpp
@@ -16,24 +16,13 @@ public:
     HardwareManager(),
     bipProvider(std::make_shared<MockBIPProvider>()),
     bopProvider(std::make_shared<MockBOPProvider>()),
-    pwmChannelProvider(std::make_shared<MockPWMChannelProvider>()),
-    bipProviderRegistrar(std::make_shared<Lineside::BIPProviderRegistrar>()),
-    bopProviderRegistrar(std::make_shared<Lineside::BOPProviderRegistrar>()),
-    pwmChannelProviderRegistrar(std::make_shared<Lineside::PWMCProviderRegistrar>()) {
-    this->bipProviderRegistrar->Register( BIPProviderId, this->bipProvider );
-    this->bopProviderRegistrar->Register( BOPProviderId, this->bopProvider );
-    this->pwmChannelProviderRegistrar->Register( PWMChannelProviderId, this->pwmChannelProvider );
+    pwmChannelProvider(std::make_shared<MockPWMChannelProvider>()) {
+    this->bipProviderRegistrar.Register( BIPProviderId, this->bipProvider );
+    this->bopProviderRegistrar.Register( BOPProviderId, this->bopProvider );
+    this->pwmcProviderRegistrar.Register( PWMChannelProviderId, this->pwmChannelProvider );
   }
 
   std::shared_ptr<MockBIPProvider> bipProvider;
   std::shared_ptr<MockBOPProvider> bopProvider;
   std::shared_ptr<MockPWMChannelProvider> pwmChannelProvider;
-
-  std::shared_ptr<Lineside::BIPProviderRegistrar> bipProviderRegistrar;
-  std::shared_ptr<Lineside::BOPProviderRegistrar> bopProviderRegistrar;
-  std::shared_ptr<Lineside::PWMCProviderRegistrar> pwmChannelProviderRegistrar;
-  
-  virtual std::weak_ptr<Lineside::BOPProviderRegistrar> GetBOPProviderRegistrar() override;
-  virtual std::weak_ptr<Lineside::BIPProviderRegistrar> GetBIPProviderRegistrar() override;
-  virtual std::weak_ptr<Lineside::PWMCProviderRegistrar> GetPWMCProviderRegistrar() override;
 };
