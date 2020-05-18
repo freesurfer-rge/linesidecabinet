3ada91575732ee439ac5fdbc55710545b32e1160
Richard Edgar <freesurfer.rge@gmail.com>
2020-05-17 21:25:29 -0400

    Putting in command line option reader

--- apps/signals/main.cpp
+++ apps/signals/main.cpp
@@ -1,6 +1,20 @@
 #include <cstdlib>
 #include <iostream>
 
-int main() {
+#include "cmdlineopts.hpp"
+
+int main(int argc, char* argv[]) {
+  try {
+    CmdLineOpts opts;
+    opts.Populate(argc, argv);
+    if( opts.helpMessagePrinted ) {
+      return EXIT_SUCCESS;
+    }
+  }
+  catch(std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  
   return EXIT_SUCCESS;
 }
