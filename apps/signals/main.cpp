40a55c1033d5b7de24347f5816138c25a706d6da
Richard Edgar <freesurfer.rge@gmail.com>
2020-05-18 22:17:08 -0400

    Add simple XML file and read it in

--- apps/signals/main.cpp
+++ apps/signals/main.cpp
@@ -1,7 +1,20 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "xml/configurationreader.hpp"
+#include "multiaspectsignalheaddata.hpp"
+
 #include "cmdlineopts.hpp"
+
+
+void CheckPWItems(const std::vector<std::shared_ptr<Lineside::PWItemData>>& pwItems) {
+  for( auto it=pwItems.begin(); it!=pwItems.end(); ++it ) {
+    auto signal = std::dynamic_pointer_cast<Lineside::MultiAspectSignalHeadData>(*it);
+    if( !signal ) {
+      throw std::runtime_error("All PWItems must be of type MultiAspectSignalHead");
+    }
+  }
+}
 
 int main(int argc, char* argv[]) {
   try {
@@ -10,6 +23,12 @@ int main(int argc, char* argv[]) {
     if( opts.helpMessagePrinted ) {
       return EXIT_SUCCESS;
     }
+
+    Lineside::xml::ConfigurationReader cr;
+
+    auto config = cr.Read(opts.configFilePath);
+    CheckPWItems(config.pwItems);
+
   }
   catch(std::exception& e) {
     std::cerr << "Error: " << e.what() << std::endl;
