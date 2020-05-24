34b67dc637740e9ece530c3e336a333afd23bd22
Richard Edgar <freesurfer.rge@gmail.com>
2020-05-24 19:50:18 -0400

    Add the GPIO input to the PiHardwareManager

--- tst/pihardwaremanagertests.cpp
+++ tst/pihardwaremanagertests.cpp
@@ -4,7 +4,7 @@
 
 BOOST_AUTO_TEST_SUITE( PiHardwareManager )
 
-BOOST_AUTO_TEST_CASE( Smoke )
+BOOST_AUTO_TEST_CASE( SmokeOutputPin )
 {
   Lineside::HardwareManagerData hmd;
 
@@ -23,6 +23,27 @@ BOOST_AUTO_TEST_CASE( Smoke )
 
   auto gpbop = dynamic_cast<Lineside::PiGPIOd::GPOutput*>(pin.get());
   BOOST_REQUIRE(gpbop);
+}
+
+BOOST_AUTO_TEST_CASE( SmokeInputPin )
+{
+  Lineside::HardwareManagerData hmd;
+  Lineside::PiGPIOd::PiHardwareManager pi(hmd);
+
+  auto bipProvider = pi.bipProviderRegistrar.Retrieve("GPIO");
+  BOOST_REQUIRE( bipProvider );
+  
+  std::string pinId = "5";
+  auto settings = std::map<std::string,std::string>();
+  settings["pud"] = "Down";
+
+  auto pin = bipProvider->GetHardware( pinId, settings );
+  BOOST_REQUIRE(pin);
+  // With the pulldown resistor, expect the pin to be off
+  BOOST_CHECK_EQUAL( pin->Get(), false );
+
+  auto gpbip = dynamic_cast<Lineside::PiGPIOd::GPInput*>(pin.get());
+  BOOST_REQUIRE(gpbip);
 }
 
 BOOST_AUTO_TEST_CASE( CheckGPIODevice )
