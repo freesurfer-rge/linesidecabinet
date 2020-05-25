b5a93e16cbcf99c40583e84ea5e41c4003828d33
R Edgar <freesurfer.rge@gmail.com>
2020-05-25 10:41:06 -0400

    Add app for testing TrackCircuitMonitor (#40)
    A simple application which enables testing of the `TrackCircuitMonitor` class and associated input pin paraphernalia. This has uncovered two issues in the XML reader, opened as issues #38 and #39 .
    
    While this has shown that the callbacks etc. are working, consideration of how often to send notifications is required.

---
+++ apps/trackcircuitmonitor/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "pigpiod/pihardwaremanager.hpp"
+#include "xml/configurationreader.hpp"
+#include "trackcircuitmonitordata.hpp"
+
+#include "pwitemmanager.hpp"
+
+#include "cmdlineopts.hpp"
+#include "stubsoftwaremanager.hpp"
+
+void CheckPWItems(const std::vector<std::shared_ptr<Lineside::PWItemData>>& pwItems) {
+  for( auto it=pwItems.begin(); it!=pwItems.end(); ++it ) {
+    auto signal = std::dynamic_pointer_cast<Lineside::TrackCircuitMonitorData>(*it);
+    if( !signal ) {
+      throw std::runtime_error("All PWItems must be of type TrackCircuitMonitor");
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  try {
+    CmdLineOpts opts;
+    opts.Populate(argc, argv);
+    if( opts.helpMessagePrinted ) {
+      return EXIT_SUCCESS;
+    }
+
+    Lineside::xml::ConfigurationReader cr;
+
+    auto config = cr.Read(opts.configFilePath);
+    CheckPWItems(config.pwItems);
+
+    auto hw = std::make_shared<Lineside::PiGPIOd::PiHardwareManager>(config.hwManager);
+    auto sw = std::make_shared<StubSoftwareManager>();
+    Lineside::PWItemManager pwItemManager( hw, sw );
+
+    pwItemManager.CreatePWItems(config.pwItems);
+
+    std::cout << "Entering main loop" << std::endl;
+    std::cout << "Type q to quit" << std::endl;
+    bool done = false;
+    while( !done ) {
+      std::string inputLine;
+      std::getline( std::cin, inputLine );
+      if( inputLine == "q" ) {
+	std::cout << "Received quit" << std::endl;
+	done = true;
+      }
+    }
+  }
+  catch(std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  return EXIT_SUCCESS;
+}
