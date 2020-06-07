e050a3b410bf54ade8f1953ac2da8941499d575f
Richard Edgar <freesurfer.rge@gmail.com>
2020-06-07 17:23:58 -0400

    Moving over the gpiopintests

--- libpigpiodpp/test/gpiopintests.cpp
+++ libpigpiodpp/test/gpiopintests.cpp
@@ -1,17 +1,15 @@
 #include <boost/test/unit_test.hpp>
 
-#include "pigpiod/gpiopin.hpp"
-
-BOOST_AUTO_TEST_SUITE( pigpiod )
+#include "pigpiodpp/gpiopin.hpp"
 
 BOOST_AUTO_TEST_SUITE( GPIOPin )
 
 BOOST_AUTO_TEST_CASE( Smoke )
 {
-  auto pm = Lineside::PiGPIOd::PiManager::CreatePiManager();
+  auto pm = PiGPIOdpp::PiManager::CreatePiManager();
 
   const int pinId = 5;
-  Lineside::PiGPIOd::GPIOPin pin(pm, pinId);
+  PiGPIOdpp::GPIOPin pin(pm, pinId);
   
   BOOST_CHECK_EQUAL( pin.getPi(), pm->getId() );
   BOOST_CHECK_EQUAL( pin.getPin(), pinId );
@@ -20,18 +18,18 @@ BOOST_AUTO_TEST_CASE( Smoke )
 BOOST_AUTO_TEST_CASE( CallBackInvoked )
 {
   const unsigned char pinId = 5;
-  auto pm = Lineside::PiGPIOd::PiManager::CreatePiManager();
+  auto pm = PiGPIOdpp::PiManager::CreatePiManager();
 
-  Lineside::PiGPIOd::GPIOPin pin(pm, pinId);
+  PiGPIOdpp::GPIOPin pin(pm, pinId);
 
   bool funcCalled = false;
   bool level = false;
-  Lineside::PiGPIOd::GPIOPin::CallBackFn f = [&funcCalled,&level](bool l) {
-					       funcCalled = true;
-					       level = l;
-					     };
+  PiGPIOdpp::GPIOPin::CallBackFn f = [&funcCalled,&level](bool l) {
+				       funcCalled = true;
+				       level = l;
+				     };
 
-  pin.SetCallBack(Lineside::PiGPIOd::GPIOEdge::Either, f);
+  pin.SetCallBack(PiGPIOdpp::GPIOEdge::Either, f);
 
   // Invoke to true
   pin.InvokeCallBack(pin.getPi(), pinId, 1, 0);
@@ -44,7 +42,5 @@ BOOST_AUTO_TEST_CASE( CallBackInvoked )
   BOOST_CHECK( funcCalled );
   BOOST_CHECK( !level );
 }
-
-BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE_END()
