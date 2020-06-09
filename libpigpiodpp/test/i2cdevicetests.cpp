f154e192ca6303bc7ca8b402a8c1f1121884d543
R Edgar <freesurfer.rge@gmail.com>
2020-06-09 12:45:11 -0400

    Fix tests for a real Pi

--- libpigpiodpp/test/i2cdevicetests.cpp
+++ libpigpiodpp/test/i2cdevicetests.cpp
@@ -1,6 +1,13 @@
 #include <boost/test/unit_test.hpp>
 
+#include "pigpiodpp/pigpiodppexception.hpp"
 #include "pigpiodpp/i2cdevice.hpp"
+
+#ifdef PIGPIODPP_HAVE_PIGPIO
+const bool haveHardware = true;
+#else
+const bool haveHardware = false;
+#endif
 
 BOOST_AUTO_TEST_SUITE( I2CDevice )
 
@@ -11,14 +18,20 @@ BOOST_AUTO_TEST_CASE( Smoke )
   const unsigned int busId = 0;
   const unsigned int deviceId = 0x1;
 
-  PiGPIOdpp::I2CDevice i2cDev(pm, busId, deviceId);
-
-  BOOST_CHECK_EQUAL( i2cDev.getPi(), pm->getId() );
-  BOOST_CHECK_EQUAL( i2cDev.getI2CBus(), busId );
-  BOOST_CHECK_EQUAL( i2cDev.getI2CAddress(), deviceId );
-  BOOST_CHECK( i2cDev.getHandle() >= 0 );
+  if( haveHardware ) {
+    // Expect failure (on a bare Pi)
+    BOOST_CHECK_THROW(PiGPIOdpp::I2CDevice i2cDev(pm, busId, deviceId),
+		      PiGPIOdpp::PiGPIOdppException);
+  } else {
+    PiGPIOdpp::I2CDevice i2cDev(pm, busId, deviceId);
+    
+    BOOST_CHECK_EQUAL( i2cDev.getPi(), pm->getId() );
+    BOOST_CHECK_EQUAL( i2cDev.getI2CBus(), busId );
+    BOOST_CHECK_EQUAL( i2cDev.getI2CAddress(), deviceId );
+    BOOST_CHECK( i2cDev.getHandle() >= 0 );
+  }
 }
-
+  
 BOOST_AUTO_TEST_CASE( WriteByte )
 {
   auto pm = PiGPIOdpp::PiManager::CreatePiManager();
@@ -26,9 +39,15 @@ BOOST_AUTO_TEST_CASE( WriteByte )
   const unsigned int busId = 0;
   const unsigned int deviceId = 0x1;
 
-  PiGPIOdpp::I2CDevice i2cDev(pm, busId, deviceId);
-
-  BOOST_CHECK_NO_THROW( i2cDev.WriteByte(1, 2) );
+  if( haveHardware ) {
+    // Expect failure (on a bare Pi)
+    BOOST_CHECK_THROW(PiGPIOdpp::I2CDevice i2cDev(pm, busId, deviceId),
+		      PiGPIOdpp::PiGPIOdppException);
+  } else {
+    PiGPIOdpp::I2CDevice i2cDev(pm, busId, deviceId);
+  
+    BOOST_CHECK_NO_THROW( i2cDev.WriteByte(1, 2) );
+  }
 }
 
 BOOST_AUTO_TEST_CASE( WriteWord )
@@ -38,9 +57,15 @@ BOOST_AUTO_TEST_CASE( WriteWord )
   const unsigned int busId = 0;
   const unsigned int deviceId = 0x1;
 
-  PiGPIOdpp::I2CDevice i2cDev(pm, busId, deviceId);
+  if( haveHardware ) {
+    // Expect failure (on a bare Pi)
+    BOOST_CHECK_THROW(PiGPIOdpp::I2CDevice i2cDev(pm, busId, deviceId),
+		      PiGPIOdpp::PiGPIOdppException);
+  } else {
+    PiGPIOdpp::I2CDevice i2cDev(pm, busId, deviceId);
 
-  BOOST_CHECK_NO_THROW( i2cDev.WriteWord(4, 5) );
+    BOOST_CHECK_NO_THROW( i2cDev.WriteWord(4, 5) );
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
