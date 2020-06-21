bd61e3110b937840ead36aaa4c23b60409d041d2
Richard Edgar <freesurfer.rge@gmail.com>
2020-06-21 15:11:04 -0400

    Do a build of json-rpc-cpp

--- scripts/json-rpc-cpp-install.sh
+++ scripts/json-rpc-cpp-install.sh
@@ -3,8 +3,23 @@ set echo
 # sudo apt-get install libjsonrpccpp-dev libjsonrpccpp-tools
 
 # Install prereqs
-sudo apt-get install lubcurl-dev libmicrohttpd-dev libargtable libjsoncpp-dev libhiredis-dev catch2
+sudo apt-get install libcurl-dev libmicrohttpd-dev libargtable2-dev libjsoncpp-dev libhiredis-dev
 
 sudo updatedb
 
+# Get the sources
+cd /tmp
+git clone https://github.com/cinemast/libjson-rpc-cpp.git
 
+# Build
+mkdir -p libjson-rpc-cpp/build
+cd libjson-rpc-cpp/build
+cmake ..
+make -j 2
+
+# Test
+make test
+
+# Install
+sudo make install
+sudo ldconfig
