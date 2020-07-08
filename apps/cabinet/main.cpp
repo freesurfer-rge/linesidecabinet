47fb806975eaf6215e7c22dbe6b470df2a7f02dd
Richard Edgar <freesurfer.rge@gmail.com>
2020-07-08 04:29:52 -0400

    Add note about current status

--- apps/cabinet/main.cpp
+++ apps/cabinet/main.cpp
@@ -6,7 +6,19 @@
 
 /*
   Some sample calls
-  For some reason, only "GetTrackCircuit" is working right now
+  
+  Outstanding Issues:
+  - Only GetTrackCircuit is working due to issue in the JSON parsing
+    json-rpc-cxx is seeing the string "DoubleYellow" and not noticing that
+    it can convert that to the appropriate type. The only reason that
+    GetTrackCircuit is working is that the id argument is of type string, and
+    is converted internally by GetTrackCircuit
+  - All three methods are configured as RPC methods, rather than notifications
+    This is indicated by the fact that they have an "id" field in the request
+    and a non-void return type. This is because RPC notifications cannot even
+    return errors:
+    https://www.jsonrpc.org/specification#notification
+    Currently, the return types are all bool; should improve this
 
   curl http://localhost:8483/jsonrpc -H "Content-Type application/json" --data '{ "method":"SetMultiAspectSignal", "params":{"id":"00:1a:2b:3c", "state":"DoubleYellow", "flash":"Steady", "feather":0}, "jsonrpc": "2.0" }'
 
