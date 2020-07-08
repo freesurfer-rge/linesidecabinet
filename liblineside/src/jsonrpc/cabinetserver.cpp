94810f50aa00280b05138c2423362862a2baf1b6
Richard Edgar <freesurfer.rge@gmail.com>
2020-07-07 20:33:37 -0400

    Remove duplicated code

--- liblineside/src/jsonrpc/cabinetserver.cpp
+++ liblineside/src/jsonrpc/cabinetserver.cpp
@@ -1,26 +1,8 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
 
-
+#include "lineside/jsonrpc/jsonconvertors.hpp"
 #include "cabinetserver.hpp"
-
-namespace Lineside {
-  
-
-inline void to_json (nlohmann::json &j, const Lineside::ItemId& id) {
-  j = nlohmann::json{id.ToString()};
-}
-inline void from_json(const nlohmann::json &j, Lineside::ItemId& id) {
-  id.Parse(j.get<std::string>());
-}
-
-NLOHMANN_JSON_SERIALIZE_ENUM(Lineside::SignalState,
-			     {
-			      {Lineside::SignalState::Red, ToString(Lineside::SignalState::Red)},
-			      {Lineside::SignalState::Green, ToString(Lineside::SignalState::Green)}
-			     });
-    
-}
 
 
 namespace Lineside {
