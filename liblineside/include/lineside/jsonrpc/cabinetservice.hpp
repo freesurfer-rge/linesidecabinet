c289e444f1bb1add18d1d587511ce5fa4c17d1dd
Richard Edgar <freesurfer.rge@gmail.com>
2020-08-08 20:57:46 -0400

    Add Doxygen

--- liblineside/include/lineside/jsonrpc/cabinetservice.hpp
+++ liblineside/include/lineside/jsonrpc/cabinetservice.hpp
@@ -14,13 +14,19 @@ namespace Lineside {
     public:
       virtual ~CabinetService() {}
 
+      //! Function to set the state of a multiaspect signal
       virtual
       CabinetServiceResponse
       SetMultiAspectSignal(const Lineside::ItemId id,
 			   const Lineside::SignalState state,
 			   const Lineside::SignalFlash flash,
 			   const unsigned int feather) = 0;
-      
+
+      //! Function to set the state of a multiaspect signal
+      /*!
+	This is a dupicate with string arguments in case there are issues
+	with converting the custom types.
+       */
       virtual
       CabinetServiceResponse
       SetMultiAspectSignal(const std::string id,
@@ -28,18 +34,30 @@ namespace Lineside {
 			   const std::string flash,
 			   const unsigned int feather) = 0;
 
+      //! Function to set the state of a turnout
       virtual
       CabinetServiceResponse
       SetTurnout(const Lineside::ItemId id,
 		 const Lineside::TurnoutState state) = 0;
       
+      //! Function to set the state of a turnout
+      /*!
+	This is a dupicate with string arguments in case there are issues
+	with converting the custom types.
+       */
       virtual
       CabinetServiceRespose
       SetTurnout(const std::string id,
 		 const std::string state) = 0;
 
+      //! Function to get the state of a track circuit
       virtual bool GetTrackCircuit(const Lineside::ItemId id) = 0;
 
+      //! Function to get the state of a track circuit
+      /*!
+	This is a dupicate with string arguments in case there are issues
+	with converting the custom types.
+       */
       virtual bool GetTrackCircuit(const std::string id) = 0;
     };
   }
