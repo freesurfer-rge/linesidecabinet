258faee35be1ae17e42f2aa592e716f9d2d0d08d
Richard Edgar <freesurfer.rge@gmail.com>
2020-10-21 20:56:58 -0400

    More trimming

--- liblineside/src/jsonrpc/cabinetserviceimpl.cpp
+++ liblineside/src/jsonrpc/cabinetserviceimpl.cpp
@@ -29,17 +29,6 @@ namespace Lineside {
     }
 
     CabinetServiceResponse
-    CabinetServiceImpl::SetMultiAspectSignalString(const std::string id,
-						   const std::string state,
-						   const std::string flash,
-						   const unsigned int feather ) {
-      auto idObj = Lineside::Parse<ItemId>(id);
-      auto stateEnum = Lineside::Parse<Lineside::SignalState>(state);
-      auto flashEnum = Lineside::Parse<Lineside::SignalFlash>(flash);
-      return this->SetMultiAspectSignal(idObj, stateEnum, flashEnum, feather);
-    }
-
-    CabinetServiceResponse
     CabinetServiceImpl::SetTurnout(const Lineside::ItemId id,
 				   const Lineside::TurnoutState state) {
       std::cout << __FUNCTION__ << ": "
@@ -51,25 +40,12 @@ namespace Lineside {
       return CabinetServiceResponse::Success;
     }
 
-    CabinetServiceResponse
-    CabinetServiceImpl::SetTurnoutString(const std::string id,
-					 const std::string state) {
-      auto idObj = Lineside::Parse<ItemId>(id);
-      auto stateEnum = Lineside::Parse<Lineside::TurnoutState>(state);
-      return this->SetTurnout(idObj, stateEnum);
-    }
-
     bool CabinetServiceImpl::GetTrackCircuit(const Lineside::ItemId id) {
       std::cout << __FUNCTION__ << ": "
 		<< id << std::endl;
       Lineside::PWItemModel& item = this->pwim->GetPWItemModelById(id);
       Lineside::TrackCircuitMonitor& tcm = dynamic_cast<Lineside::TrackCircuitMonitor&>(item);
       return tcm.GetState();
-    }
-
-    bool CabinetServiceImpl::GetTrackCircuitString(const std::string id) {
-      auto idObj = Lineside::Parse<ItemId>(id);
-      return this->GetTrackCircuit(idObj);
     }
   }
 }
