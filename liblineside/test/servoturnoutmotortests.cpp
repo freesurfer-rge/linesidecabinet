9a35a0acba1c26e6e3ab38864cf98e70693c3578
Richard Edgar <freesurfer.rge@gmail.com>
2020-11-24 16:10:11 -0500

    Add logging of updates to MockPWMChannel

--- liblineside/test/servoturnoutmotortests.cpp
+++ liblineside/test/servoturnoutmotortests.cpp
@@ -130,7 +130,6 @@ BOOST_AUTO_TEST_CASE(SetStateAndStateChange)
 }
 
 
-#if 0
 BOOST_AUTO_TEST_CASE(SetCurvedAndStraight)
 {
   const unsigned int notifySrcId = 1235;
@@ -139,7 +138,7 @@ BOOST_AUTO_TEST_CASE(SetCurvedAndStraight)
   const Lineside::ItemId id(10);
   const unsigned int straight = 10;
   const unsigned int curved = 113;
-  const std::string controller = "MockPWMController";
+  const std::string controller =  Tendril::Mocks::PWMCProviderId;
   const std::string controllerData = "07";
   
   Lineside::ServoTurnoutMotorData stmd;
@@ -221,6 +220,5 @@ BOOST_AUTO_TEST_CASE(SetCurvedAndStraight)
     BOOST_CHECK_LE( prev.second, curved );
   }
 }
-#endif
 
 BOOST_AUTO_TEST_SUITE_END()
