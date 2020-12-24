264aa15378836b2617f8b390ab640a649ad97280
Richard Edgar <freesurfer.rge@gmail.com>
2020-12-24 11:24:52 -0500

    Starting to think about the next reader

---
+++ liblineside/test/xml/directdrivesn74x595datareadertests.cpp
@@ -0,0 +1,23 @@
+#include <boost/test/unit_test.hpp>
+#include <xercesc/parsers/XercesDOMParser.hpp>
+
+#include "xmlutils.hpp"
+
+#include "tendril/devices/device.hpp"
+#include "lineside/xml/xercesguard.hpp"
+
+// ==============
+
+BOOST_AUTO_TEST_SUITE( xml )
+
+BOOST_AUTO_TEST_SUITE( DirectDriveSN74x595DataReader )
+
+BOOST_AUTO_TEST_CASE( SmokeReader )
+{
+  BOOST_FAIL("Need to write");
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
